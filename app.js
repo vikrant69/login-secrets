4491561b807fc71326c4c8f23bc949491b22ebe1
Vikrant <153vikrantkumar@gmail.com>
2022-03-25 17:16:36 +0530

    Level 3 encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -18,10 +18,9 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
-
-
 const User = mongoose.model('User', userSchema);
+
+console.log(md5('V8k4a!5#123'));
 
 app.get('/', (req, res)=>{
     res.render('home');
@@ -39,7 +38,7 @@ app.get('/register', (req, res)=>{
 app.post('/register', (req, res)=>{
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save((err)=>{
@@ -52,7 +51,7 @@ app.post('/register', (req, res)=>{
 
 app.post('/login', (req, res)=>{
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, (err, foundUser)=>{
         if(err)console.log(err);
