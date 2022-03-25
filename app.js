cabc7aa326c94a36965473172b90e3fc36415e77
Vikrant <153vikrantkumar@gmail.com>
2022-03-25 16:30:52 +0530

    level 2 encryption

---
+++ app.js
@@ -0,0 +1,72 @@
+require('dotenv').config();  // this line should be put on top to cnofig env
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect('mongodb://0.0.0.0:27017/userDB');
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
+
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res)=>{
+    res.render('home');
+});
+
+app.get('/login', (req, res)=>{
+    res.render('login');
+});
+
+app.get('/register', (req, res)=>{
+    res.render('register');
+});
+
+
+app.post('/register', (req, res)=>{
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save((err)=>{
+        if(err)console.log(err);
+        else{
+            res.render('secrets');
+        }
+    })
+});
+
+app.post('/login', (req, res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, (err, foundUser)=>{
+        if(err)console.log(err);
+        else{
+            if(foundUser){
+                if(password === foundUser.password){
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+});
+
+
+app.listen(3000, ()=>{
+    console.log('Server is running on port 30000');
+});
